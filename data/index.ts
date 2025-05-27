08b7090af2bc2eccdf72131997d7177f9b8a6e91
Nitin Peter <154044708+NitinS47@users.noreply.github.com>
2025-05-27 17:38:10 +0530

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -1,6 +1,6 @@
 export const navItems = [
   { name: "About", link: "#about" },
-  { name: "Projects", link: "#project" },
+  { name: "Projects", link: "#projects" },
   { name: "Testimonials", link: "#testimonials" },
   { name: "Contact", link: "#contact" },
 ];
